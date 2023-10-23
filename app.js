f5890194470a15002d7e38258faa97afb9c1a543
DSummers2001 <dillon.summers2001@gmail.com>
2023-10-23 16:30:47 -0700

    More work on the setup and adding in an upgrade

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
 let count = 0;
+let clickValue = 1;
+let autoDonutRate = 0;
+let autoDonuts = 0;
 
 const donutButton = document.getElementById("donut");
 const countDisplay = document.getElementById("count");
+const upgradeSelect = document.getElementById("upgradeSelect");
+const purchaseButton = document.getElementById("purchaseButton");
 const dropdownButton = document.getElementById("dropdownButton");
 const dropdownList = document.getElementById("dropdownList");
 
+function updateDisplay(){
+    CountDisplay.textContent = count;
+    upgradeSelect.options[0].text = `Faster Clicks (10 Donuts, Current: ${clickValue}x)`;
+    upgradeSelect.options[1].text = `Auto Donuts (50 Donuts, Current: ${autoDonutRate} per second)`;
+}
+
 donutButton.addEventListener('click', () => {
-    count++;
-    countDisplay.textContent = `Cookies: ${count}`;
+    donutCount += clickValue;
+    updateDisplay();
 });
 
-dropdownButton.addEventListener('click', () => {
-    dropdownList.classList.toggle('show');
+upgradeSelect.addEventListener('change', () =>{
+    const selectedValue = upgradeSelect.value;
+        if (donutCount >= selectedValue){
+            if (selectedValue === '1'){
+                clickValue +=1;
+            } else if (selectedValue === '5'){
+                autoDonutRate += 1;
+                autoDonuts += autoDonutRate;
+            }
+            donutCount -= selectedValue;
+            upgradeSelect.selectedIndex = 0;
+            updateDisplay();
+        }
 });
 
-window.addEventListener('click', (event) => {
-    if (!event.target.matches('#dropdownButton')){
-        dropdownList.classList.remove('show');
-    }
-});
+function autoDonutClick(){
+    donutCount += autoDonuts;
+    updateDisplay();
+}
+
+setInterval(autoDonutClick, 1000);
+
+updateDisplay();
