e80b0437d286141ebd2e190a30d9ce4739ec1b51
DSummers2001 <dillon.summers2001@gmail.com>
2023-10-24 18:45:26 -0700

    still trying to get the auto clicker to work

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
 let donut = document.querySelector('.donutCost');
-let parsedDonut = parseFloat(donut.innerHTML)
+let parsedDonut = parseFloat(donut.innerHTML);
 
 let clickerCost = document.querySelector('.clickerCost');
-let parsedClickerCost = parseFloat(clickerCost.innerHTML)
+let parsedClickerCost = parseFloat(clickerCost.innerHTML);
+
+let autoClickerLevel = document.getElementById('.clickerLevel');
+let autoClickerInterval;
+
+function purchaseItem(){
+    if (parsedDonut >= parsedClickerCost){
+        alert('Item purchased');
+        parsedDonut -= parsedClickerCost;
+        donut.innerHTML = parsedDonut;
+
+        parsedClickerCost *= 1.2;
+        clickerCost.innerHTML = parsedClickerCost;
+
+        autoClickerLevel++;
+
+        clearInterval(autoClickerInterval);
+        autoClickerInterval = setInterval(incrementDonut, 1000 / autoClickerLevel);
+    } else {
+        alert('Insufficient balance to purchase this item.');
+    }
+}
 
 function buyClicker(){
     if (parsedDonut >= parsedClickerCost){
-        parsedDonut -= parsedClickerCost
-        donut.innerHTML = parsedDonut
+        parsedDonut -= parsedClickerCost;
+        donut.innerHTML = parsedDonut;
     }
 }
 
 function incrementDonut(){
-    parsedDonut += 1
-    donut.innerHTML = parsedDonut
+    parsedDonut += 1;
+    donut.innerHTML = parsedDonut;
 }
